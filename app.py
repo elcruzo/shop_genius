bab2c6960670ba509c95637de61624e7c2b3f55c
ElCruzo <ayomideadekoya266@gmail.com>
2023-09-20 17:21:09 -0400

    Added some functionality

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'elcruzo_secret_key'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.String(80), primary_key=True)
+    password = db.Column(db.String(80))
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    current_price = db.Column(db.Float)
+    target_price = db.Column(db.Float)
+    user_id = db.Column(db.String(80), db.ForeignKey('user.id'))
+    
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+users = {'user1': {'password': 'password123'}}
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 products = [
     {"id": 1, "name": "Laptop", "current_price": 1000.0, "target_price": 900.0},
@@ -11,10 +45,27 @@ products = [
 def index():
     return render_template('index.html', products=products)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
 @app.route('/track/<int:product_id>', methods=['POST'])
+@login_required
 def track_product(product_id):
     target_price = float(request.form['target_price'])
-    # Logic to save the target price and send price alerts goes here
     return redirect('/')
 
 if __name__ == '__main__':
